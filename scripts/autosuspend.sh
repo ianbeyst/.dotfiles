95b9ba0e395136c4ceccc73b5718b828909600d9
ian <42294085+ianbeyst@users.noreply.github.com>
2020-04-04 12:09:20 +0200

    Various tweaks

--- scripts/autosuspend.sh
+++ scripts/autosuspend.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
     read -r status capacity
+
+    if [ "$status" = Discharging -a "$capacity" -lt 11 ]; then
+        su -l -s /bin/sh io -c 'export DISPLAY=:0; zenity --warning --text="SoC ≤ 0.1" --icon-name=""'
+    fi
+
     if [ "$status" = Discharging -a "$capacity" -lt 6 ]; then
         logger "Battery critical state of charge, suspending..."
         su -l -s /bin/sh io -c 'export DISPLAY=:0; slock' &
--- scripts/webcamshot.sh
+++ scripts/webcamshot.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-v4l2-ctl --set-ctrl=hue=-35
 v4l2-ctl --set-ctrl=exposure_auto_priority=0
-v4l2-ctl --set-ctrl=exposure_auto=01
-v4l2-ctl --set-ctrl=white_balance_temperature_auto=0
-v4l2-ctl --set-ctrl=white_balance_temperature=6500
+v4l2-ctl --set-ctrl=exposure_auto=1
 v4l2-ctl --set-ctrl=exposure_absolute=78
+v4l2-ctl --set-ctrl=gain=0
 ffmpeg -f v4l2 -i /dev/video0 -vframes 1 /home/io/data/webcam/$(date +%s).jpg
