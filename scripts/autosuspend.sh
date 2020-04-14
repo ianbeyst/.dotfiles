f94201fa06909b10bda9f7b4fc38b3271a66e170
ian <42294085+ianbeyst@users.noreply.github.com>
2020-04-14 13:35:24 +0200

    update autosuspend script

--- scripts/autosuspend.sh
+++ scripts/autosuspend.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
     read -r status capacity
-
-    if [ "$status" = Discharging -a "$capacity" -lt 11 ]; then
-        su -l -s /bin/sh io -c 'export DISPLAY=:0; zenity --warning --text="SoC ≤ 0.1" --icon-name=""'
-    fi
-
     if [ "$status" = Discharging -a "$capacity" -lt 6 ]; then
         logger "Battery critical state of charge, suspending..."
         su -l -s /bin/sh io -c 'export DISPLAY=:0; slock' &
         pm-suspend
+    elif [ "$status" = Discharging -a "$capacity" -lt 11 ]; then
+        su -l -s /bin/sh io -c 'export DISPLAY=:0; zenity --warning --text="SoC ≤ 0.1" --icon-name=""' &
     fi
 }
