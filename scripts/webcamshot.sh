7aa2e529cb812b011f46ef74c528d72facdd97b5
ian <42294085+ianbeyst@users.noreply.github.com>
2021-02-07 22:46:54 +0100

    Remove manual exposure in webcam shots

--- scripts/webcamshot.sh
+++ scripts/webcamshot.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-v4l2-ctl --set-ctrl=exposure_auto_priority=0
-v4l2-ctl --set-ctrl=exposure_auto=1
-v4l2-ctl --set-ctrl=exposure_absolute=78
-v4l2-ctl --set-ctrl=gain=0
 ffmpeg -f v4l2 -i /dev/video0 -vframes 1 /home/io/data/webcam/$(date +%s).jpg
